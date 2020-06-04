dffddcf48fd010e8071cf85b691ffa437e7c5e69
Melissa Kwan <>
2020-06-03 20:44:36 -0700

    Add header image

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+/** @jsx jsx */
+import { jsx, Image, Styled } from 'theme-ui'
+import Container from './components/container'
 import './App.css';
+import logo from './assets/coverphoto.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div
+        sx={{
+          backgroundColor: '#040404',
+          minHeight: ['5em', '15em'],
+          justifyContent: 'center',
+          textAlign: 'center',
+          padding: '0.5em',
+        }}
+      >
+        <Image
+          src={logo}
+          sx={{
+            maxHeight:['5em', '15em'],
+          }}
+        />
+      </div>
+      <Container>
+        <h1>Hello?</h1>
+        <h2>This is a secondary header</h2>
+        Hello?
+        Hi everyone
+        <b>Hi</b>
+        <p>Trying again</p>
+      </Container>
     </div>
+    
   );
 }
 
