0a7db7329cc0d79634bcff0152261cf297169bb9
Melissa Kwan <>
2020-06-07 13:50:04 -0700

    Add Equal Justice Initiative

--- src/components/infographic.js
+++ src/components/infographic.js
@@ -14,19 +14,23 @@ const Infographic = ({loading, total, count }) => {
         }
       </Styled.h1>
       <Styled.p sx={{ marginTop: '0.25em', fontSize: 3 }}>
-        {'total donated to '}
+        {'total raised for '}
         <Styled.a target='_blank' href='https://blacklivesmatter.com/'>Black Lives Matter</Styled.a>
         {', '}
         <Styled.a target='_blank' href='https://bailproject.org/'>The Bail Project</Styled.a>
         {', and '}
-        <Styled.a target='_blank' href='https://www.joincampaignzero.org/'>Campaign Zero.</Styled.a>
+        <Styled.a target='_blank' href='https://www.eji.org/'>Equal Justice Initiative.</Styled.a>
       </Styled.p>
       <Text variant='infographic'><b>{student}</b>{` student and alumni contribution${student === 1 ? '' : 's'}`}</Text>
       <Text variant='infographic'><b>{faculty}</b>{` faculty contribution${faculty === 1 ? '' : 's'}`}</Text>
       <Text variant='infographic'><b>{parent}</b>{` parent contribution${parent === 1 ? '' : 's'}`}</Text>
       <Text variant='infographic'><b>{community}</b>{` community contribution${community === 1 ? '' : 's'}`}</Text>
       <br />
-      <Styled.h3><Styled.a target='_blank' href='https://bit.ly/harkerblm'>Resources for allyship</Styled.a></Styled.h3>
+      <Styled.h3>
+        <Styled.a target='_blank' href='https://bit.ly/harkerblm'>Learn, petition, donate</Styled.a>
+        {`   •   `}
+        <Styled.a target='_blank' href='https://docs.google.com/document/d/1BRlF2_zhNe86SGgHa6-VlBO-QgirITwCTugSfKie5Fs'>How to be anti-racist</Styled.a> 
+      </Styled.h3>
       <br />
     </div>
   )
--- src/components/letter.js
+++ src/components/letter.js
@@ -15,12 +15,25 @@ const Letter = () => {
         {', and '}
         <Styled.a target='_blank' href='https://www.eji.org/'>Equal Justice Initiative.</Styled.a>
         {' Black Lives Matter drives the cultural movement for Black justice, The Bail Project combats mass incarceration, and Equal Justice Initiative '}
-        {'researches policy solutions to end police brutality.'}
+        {'challenges racial and economic injustice through public education and criminal justice reform.'}
       </Styled.p>
       <Styled.p>If you'd like to spread awareness about this fundraiser, here is the <Styled.a target='_blank' href='https://www.facebook.com/events/273772077332886'>Facebook event.</Styled.a></Styled.p>
       <Styled.p>Please also read the <Styled.a target='_blank' href='https://bit.ly/harkerblm'>educational resources</Styled.a> to help you learn, protest, and use your voice to help others do the same.</Styled.p>
       <Styled.p>You can either Venmo your donation to <Styled.a target='_blank' href='https://venmo.com/harkerblm'>@HarkerBLM</Styled.a> or donate below.</Styled.p>
       <Styled.p>Thank you for supporting these worthy causes.</Styled.p>
+      <Styled.p
+        sx={{
+          fontSize: 0,
+          color: '#111111 !important',
+          mt: 2,
+        }}
+      >
+          * The Harker Black Solidarity Fundraiser is dedicated to supporting the Black community in a way that is informed by BIPOC leaders across the country.
+          As Campaign Zero has received criticism for its data analysis and the implications of its #8CantWait campaign, we decided to redirect those funds
+          to the Equal Justice Initiative, which provides legal support to those who have been illegally convicted, unfairly
+          sentenced, or abused in jails and prisons. If you would like us to refund your donation either in part or in full, please email us at
+          <Styled.a href="mailto:HarkerBLM@gmail.com"> HarkerBLM@gmail.com</Styled.a>.
+      </Styled.p>
     </div>
   )
 }
